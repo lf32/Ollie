acb4d80c7cf55586f2e9213ee6e8c86c5e11e453
Akhil Raj <lf32.dev@gmail.com>
2022-01-01 00:00:00 +0000

    Initialize interface with a banner
    Signed-off-by: Akhil Raj <lf32.dev@gmail.com>

---
+++ Ollie.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+#
+#
+#
+#
+banner() {
+echo ""
+echo "############################ Ollie #############################" 
+echo "#							       #"
+echo "#		  mmmm  \"\"#    \"\"#      \"		       #"
+echo "#		 m'  'm   #      #    mmm     mmm	       #"
+echo "#		 #    #   #      #      #    #\"  #	       #"
+echo "#		 #    #   #      #      #    #\"\"	       #"
+echo "#		  #mm#    \"mm   \"mm   mm#mm  \"#mm\"	       #"
+echo "#							       #"
+echo "#  ~ A Command Line Interface for generating Reverse Shells ~  #"
+echo "# 							       #"
+echo "#                	   by lf32 			       #"
+echo "#                	   				       #"
+echo "############################ Ollie #############################"
+}
+banner
